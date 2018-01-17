b3b50cdee7fbe5767d2fa3617fb6c65365fec94d
Thorsten Pohlert <thorsten.pohlert@gmx.de>
2018-01-17 23:05:17 +0000

    version 4.2

---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+.onAttach <- function(libname, pkgname){
+    packageStartupMessage(c("PMCMR is superseded by PMCMRplus and will be no ",
+                            "longer maintained. You may wish to install ",
+                            "PMCMRplus instead."))
+}
